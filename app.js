84ae59b1bb41719f2e6275fc37812ef33c18456a
SteveO <stinkypiece@yahoo.com>
2020-12-04 11:30:57 -0800

    env key

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
-
+const path = require('path');
 
 const app = express();
 
 const key = process.env.REACT_APP_KEY
 
+app.use(express.static(path.join(__dirname, 'client/build')))
+
 app.get('/api', (req, res) => {
   res.send({ key })
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
 const PORT = process.env.PORT || 5000;
